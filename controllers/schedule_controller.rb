257db6821e9283da7fff5bb34811fcb42a24cbc0
Chris <cbrown605@googlemail.com>
2017-02-28 09:48:25 +0000

    working on the edit file

--- controllers/schedule_controller.rb
+++ controllers/schedule_controller.rb
@@ -20,19 +20,23 @@ post "/schedules" do
   erb(:"schedule/create")
 end
 
-
-
-get "/schedules/:id/edit" do 
+get "/schedules/:id/employee/:employee_id/edit" do 
 @schedules = Schedule.find(params[:id])
 @employees = Employee.all
 @shifts = Shift.all
+@employee_id = params[:employee_id].to_i
 erb(:"schedule/edit")
 end 
 
-# get "/schedules/:id/edit" do 
-#   @schedules = Schedule.find(params{[:id]})
-#   erb (:"schedules/show")
-#   end
+#/schedules/:id/employee/:employee_id/shift/:shift_id/edit
+
+#@shift_id = [params]
+
+get "/schedules/:id" do
+  schedule = Schedule.new(params)
+  schedule.update
+  erb(:"schedule/index")
+end
 
 
 
